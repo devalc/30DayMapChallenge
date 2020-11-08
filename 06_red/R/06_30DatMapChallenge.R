2a0d5c1452025b398255e75e0ace2c73c275047c
devalc <deva4998@vandals.uidaho.edu>
2020-11-07 21:44:12 -0800

    day 6

--- 06_red/R/06_30DatMapChallenge.R
+++ 06_red/R/06_30DatMapChallenge.R
@@ -6,7 +6,7 @@
 ##
 ## Author: Chinmay Deval
 ##
-## Created On: 2020-11-03
+## Created On: 2020-11-06
 ##
 ## Copyright (c) Chinmay Deval, 2020
 ## Email: chinmay.deval91@gmail.com
@@ -23,79 +23,38 @@ cat("\014")
 
 ## ----------------------------------Load packages---------------------------------------##
 #load packages
-library(tidyverse)
-library(osmdata)
-library(sf)
-library(cowplot)
-library(basemapR)
+library(osmplotr)
+
+## --------------------------------------------------------------------------------------##
+#Pune
+pune_bbox<- get_bbox(c(73.69389, 18.36388, 74.01505, 18.68203)) 
+pune_H <- extract_osm_objects (key = 'highway', bbox = pune_bbox)
+pune_HP <- extract_osm_objects (key = 'highway', value = 'primary', bbox = pune_bbox)
+
+#Utrecht
+utrecht_bbox<- get_bbox(c(5.003902, 52.025269,  5.196554, 52.136901 ))
+utrecht_H <- extract_osm_objects (key = 'highway', bbox = utrecht_bbox)
+utrecht_HP <- extract_osm_objects (key = 'highway', value = 'primary', bbox = utrecht_bbox)
+
+## --------------------------------------------------------------------------------------##
+#Pune map
+map <- osm_basemap (bbox = pune_bbox, bg = 'gray20')
+map <- add_osm_objects (map, pune_H, col = '#800000',
+                        size = 0.1)
+map <- add_osm_objects (map, pune_HP, col = '#ff4d4d', size =0.2)
+print_osm_map (map, filename = "./06_red/R/RoadNetworkPune.png", device = "png",
+               width = 10, units = "cm")
+# print_osm_map (map)
+
+## --------------------------------------------------------------------------------------##
+#Utrecht map
+mapU <- osm_basemap (bbox = utrecht_bbox, bg = 'gray20')
+mapU <- add_osm_objects (mapU, utrecht_H, col = '#800000',
+                        size = 0.1)
+mapU <- add_osm_objects (mapU, utrecht_HP, col = '#ff4d4d', size =0.2)
+
+print_osm_map (mapU, filename = "./06_red/R/RoadNetworkUtrecht.png", device = "png",
+               width = 10, units = "cm")
+# print_osm_map (mapU)
 
 
-#building the query
-utreach_q <- getbb("Utrecht") %>%
-  opq() %>%
-  add_osm_feature("building")
-
-str(utreach_q) #query structure
-
-Utrecht <- osmdata_sf(utreach_q)
-Utrecht
-
-utrecht_centers <- st_centroid(Utrecht$osm_points)
-
-pune_centers <- st_centroid(Pune$osm_points)
-
-# create bbox from our nc layer and expand it to include more area above/below
-bbox_pune <- expand_bbox(st_bbox(pune_centers), X = 0, Y = 150000)
-bbox_utrecht <- expand_bbox(st_bbox(utrecht_centers), X = 0, Y = 150000)
-
-
-#building the query
-Pune_q <- getbb("Pune") %>%
-  opq() %>%
-  add_osm_feature("building")
-
-str(Pune_q) #query structure
-
-Pune <- osmdata_sf(Pune_q)
-Pune
-
-
-
-#final map
-Utrecht_plt<- ggplot(Utrecht$osm_points )+
-  geom_sf(colour = "#9c0808",
-          # fill = "#08306b",
-          alpha = .5,
-          size = 0.1,
-          shape = 21)+
-  theme_void()
-
-
-#final map
-Pune_plt<-ggplot(Pune$osm_points )+
-  geom_sf(colour = "#9c0808",
-          # fill = "#08306b",
-          alpha = .2,
-          size = 0.1,
-          shape = 21)+
-  theme_void()
-
-a<- Utrecht$osm_points[sample(nrow(Utrecht$osm_points), 300), ]
-
-
-
-ggplot(a)+
-  base_map(bbox_utrecht, increase_zoom = 2, basemap = 'google-road') +
-  geom_sf(colour = "#9c0808",
-          # fill = "#08306b",
-          alpha = .5,
-          size = 0.1,
-          shape = 21) +
-  coord_sf(datum = NA,
-           xlim = c(73.69389, 74.01505),
-           ylim = c(17.01469, 20.03122 )) +
-  theme_minimal() +
-  labs(caption = 'map data \uA9 2020 Google')
-
-+
-  theme_void()
